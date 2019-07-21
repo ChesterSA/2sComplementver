da5d7e05275789f70a05e3b7df73a12077059614
ChesterSA <c.swannauger@gmail.com>
2019-07-21 16:08:13 +0100

    Initial Git creation as well as fixing logic errors
    Signed-off-by: ChesterSA <c.swannauger@gmail.com>

---
+++ src/pkg2scomplementver/Main.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2scomplementver;
+
+/**
+ *
+ * @author cswan
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        String strA = "1100";
+        String strB = "1111";
+
+        char[] a = strA.toCharArray();
+        char[] b = strB.toCharArray();
+
+        if (a[0] != b[0])
+        {
+                System.out.println("No overflow");
+        }
+        else
+        {
+            if (a[0] == '0')
+            {
+                if (a[1] == b[1] && a[0] != a[1])
+                {
+                    System.out.println("Overflow1");
+                }
+                else if (a[2] == b[2] && a[1] != b[1] && a[0] != a[2])	
+                {
+                    System.out.println("Overflow2");
+                }
+                else if ((a[3] == b[3]) && (a[2] != b[2]) && (a[1] != b[1]) && (a[0] != a[3]))
+                {
+                    System.out.println("Overflow3");
+                }
+                else
+                {
+                    System.out.println("No overflow");
+                }
+            }
+            else
+            {
+                if (a[0] != a[1] || b[0] != b[1])
+                {
+                    System.out.println("Overflow4");
+                }
+                else if (a[0] == a[2] || b[0] == b[2])
+                {
+                    System.out.println("Overflow5");
+                }
+                else
+                {
+                    System.out.println("No overflow");
+                }
+            }
+        }
+    }
+}
